3ea8b3d5f3ded3153b2078aeb8cdd74358dbb5a5
Robert Burden <robert.burden@acquia.com>
2017-05-23 16:35:19 -0400

    Project Settings Update

---
+++ Assets/Move.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+ var rotation : Quaternion;
+ var radius = Vector3(1,0,0);
+ var currentRotation = 0.0;
+ function Update()
+ {
+     currentRotation += Input.GetAxis("Horizontal")*Time.deltaTime*100;
+     currentRotation ++;
+     rotation.eulerAngles = Vector3(0, currentRotation, 0);
+     transform.position = rotation * radius;
+ }
