917f8cc598149d600c2b87debf50235cb561ed0f
Yuan Xie <yuan.xie@acquia.com>
2017-05-25 14:09:47 -0400

    Change all double quotes to single quotes.

--- SDK/DecisionApi/JavaScriptWeb/decision-api.js
+++ SDK/DecisionApi/JavaScriptWeb/decision-api.js
@@ -2,34 +2,34 @@ var AcquiaLiftDecisionApi = AcquiaLiftDecisionApi || {};
 
 AcquiaLiftDecisionApi.getCapture = AcquiaLiftDecisionApi.getCapture || function (captureConfig) {
   var defaultCaptureConfig = {
-    "event_name":"Content View",
-    "event_source":"Web",
-    "content_title":"Holodeck content",
-    "url":"http://holodeck.com",
-    "referral_url":"",
-    "user_agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3109.0 Safari/537.36",
-    "client_timezone":"240",
-    "javascript_version":"1.10.4",
-    "engagement_score":1,
-    "content_type":"page",
-    "page_type":"node page",
-    "post_id":"46",
-    "published_date":"1495650236",
-    "author":"admin"
+    'event_name':'Content View',
+    'event_source':'Web',
+    'content_title':'Holodeck content',
+    'url':'http://holodeck.com',
+    'referral_url':'',
+    'user_agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3109.0 Safari/537.36',
+    'client_timezone':'240',
+    'javascript_version':'1.10.4',
+    'engagement_score':1,
+    'content_type':'page',
+    'page_type':'node page',
+    'post_id':'46',
+    'published_date':'1495650236',
+    'author':'admin'
   };
   return Object.assign(defaultCaptureConfig, captureConfig);
 };
 
 // Get a payload.
 AcquiaLiftDecisionApi.getPayload = AcquiaLiftDecisionApi.getPayload || function (scenario, payloadConfig) {
-  var url = "http://lift3shay2ws7hd7d5ke.devcloud.acquia-sites.com/host/" + scenario;
+  var url = 'http://lift3shay2ws7hd7d5ke.devcloud.acquia-sites.com/host/' + scenario;
   var defaultPayloadConfig = {
-    "identity_source":"tracking",
-    "identity":"HackathonIdentity00001",
-    "identity_expiry":Date.now(),
-    "touch_identifier":Math.random().toString(36).substring(2),
-    "url":url,
-    "source":"https://lift3assets.dev.lift.acquia.com/latest/lift.js"
+    'identity_source':'tracking',
+    'identity':'HackathonIdentity00001',
+    'identity_expiry':Date.now(),
+    'touch_identifier':Math.random().toString(36).substring(2),
+    'url':url,
+    'source':'https://lift3assets.dev.lift.acquia.com/latest/lift.js'
   };
   return Object.assign(defaultPayloadConfig, payloadConfig);
 };
